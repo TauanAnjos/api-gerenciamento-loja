225021409cc805fad460842e619855ab9ecc55a6
tauandosanjosnascimento <tauandosanjosnascimento@gmail.com>
2025-05-01 16:11:45 -0300

    feat(repositories: Vendedor) Adicionado metodos para validar nome e buscar nome de vendedor

--- api-gerenciamento-loja/src/main/java/com/tauan/teste_tecnico/api_gerenciamento_loja/repositories/VendedorRepository.java
+++ api-gerenciamento-loja/src/main/java/com/tauan/teste_tecnico/api_gerenciamento_loja/repositories/VendedorRepository.java
@@ -3,7 +3,13 @@ package com.tauan.teste_tecnico.api_gerenciamento_loja.repositories;
 import com.tauan.teste_tecnico.api_gerenciamento_loja.models.VendedorModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface VendedorRepository extends JpaRepository<VendedorModel, UUID> {
+
+    boolean existsByNome(String nome);
+
+    Optional<VendedorModel> findByNome(String nome);
 }
