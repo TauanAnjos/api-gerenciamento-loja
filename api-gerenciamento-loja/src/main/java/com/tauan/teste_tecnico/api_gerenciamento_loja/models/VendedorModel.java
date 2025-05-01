9c6cb5925499f569ff9bab7b5192a61fdab2b501
tauandosanjosnascimento <tauandosanjosnascimento@gmail.com>
2025-05-01 16:10:54 -0300

    feat(model:vendedor) Criado metodo para mapear para dtoResponse

--- api-gerenciamento-loja/src/main/java/com/tauan/teste_tecnico/api_gerenciamento_loja/models/VendedorModel.java
+++ api-gerenciamento-loja/src/main/java/com/tauan/teste_tecnico/api_gerenciamento_loja/models/VendedorModel.java
@@ -1,5 +1,6 @@
 package com.tauan.teste_tecnico.api_gerenciamento_loja.models;
 
+import com.tauan.teste_tecnico.api_gerenciamento_loja.rest.dtos.VendedorDtoResponse;
 import jakarta.persistence.*;
 
 import java.util.UUID;
@@ -25,5 +26,41 @@ public class VendedorModel {
         this.nome = nome;
         this.email = email;
         this.senha = senha;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public VendedorDtoResponse toDtoResponse(){
+        return new VendedorDtoResponse(this.nome,this.email);
     }
 }
