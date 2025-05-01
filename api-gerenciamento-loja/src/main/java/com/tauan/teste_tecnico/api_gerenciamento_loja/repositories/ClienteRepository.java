6bca59694e7c5f86ab52a648bafea53e0e40ff60
tauandosanjosnascimento <tauandosanjosnascimento@gmail.com>
2025-05-01 15:43:43 -0300

    feat(repositories): Criado busca por email e cpf para cleinte

--- api-gerenciamento-loja/src/main/java/com/tauan/teste_tecnico/api_gerenciamento_loja/repositories/ClienteRepository.java
+++ api-gerenciamento-loja/src/main/java/com/tauan/teste_tecnico/api_gerenciamento_loja/repositories/ClienteRepository.java
@@ -3,7 +3,15 @@ package com.tauan.teste_tecnico.api_gerenciamento_loja.repositories;
 import com.tauan.teste_tecnico.api_gerenciamento_loja.models.ClienteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface ClienteRepository extends JpaRepository<ClienteModel, UUID> {
+    Optional<ClienteModel> findByCpf(String cpf);
+
+
+    Optional<ClienteModel> findByEmail(String email);
+
+    boolean existsByCpf(String cpf);
 }
