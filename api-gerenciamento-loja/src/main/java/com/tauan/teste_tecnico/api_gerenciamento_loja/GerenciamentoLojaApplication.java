e1d2e71778c275ab20c9b11f23e4248fd2803ed1
tauandosanjosnascimento <tauandosanjosnascimento@gmail.com>
2025-05-01 12:17:55 -0300

    Projeto Spring Boot - Teste Técnico

---
+++ api-gerenciamento-loja/src/main/java/com/tauan/teste_tecnico/api_gerenciamento_loja/GerenciamentoLojaApplication.java
@@ -0,0 +1,13 @@
+package com.tauan.teste_tecnico.api_gerenciamento_loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoLojaApplication.class, args);
+	}
+
+}
---
+++ api-gerenciamento-loja/src/test/java/com/tauan/teste_tecnico/api_gerenciamento_loja/GerenciamentoLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tauan.teste_tecnico.api_gerenciamento_loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
